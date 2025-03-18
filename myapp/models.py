b40907c00823551f1e08a2f785e52d6eacf238a8
joel-briones-B-Low <joellbriones701@gmail.com>
2025-03-17 22:24:19 -0600

    modelos y tablas sqllite3

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    nombre = models.CharField(max_length=200)
+
+class Task(models.Model):
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
