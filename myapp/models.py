8b13efd90e173b7f0925b4773bbd0ac680bb7664
joel-briones-B-Low <joellbriones701@gmail.com>
2025-03-18 23:43:02 -0600

    se reinicia todo JAJAJ

--- myapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Project(models.Model):
-    nombre = models.CharField(max_length=200)
-
-class Task(models.Model):
-    titulo = models.CharField(max_length=200)
-    descripcion = models.TextField()
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
